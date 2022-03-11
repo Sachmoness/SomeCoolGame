b9aef59504d03321145a301aa77dcd852d12edce
Sachin <sachinlaloo@gmail.com>
2022-03-11 18:50:59 +0200

    Fixed Jumping Issue
    performed Jump in Update rather than fixedUpdate works better.

--- CoolGame/Assets/Scripts/PlayerMovement.cs
+++ CoolGame/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     private bool _grounded; // keep track of whether player is in the air or on a platform
     private Rigidbody2D _playerBody;
 
-    private const float _jumpHeight = 2.0f;
+    private const float _jumpHeight = 7.0f;
     private const float _movementSpeed = 15.0f;
 
     // Start is called before the first frame update
@@ -22,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         GetUserInput();
+        Jump();
     }
 
     void FixedUpdate() {
@@ -33,10 +34,6 @@ public class PlayerMovement : MonoBehaviour
             _playerBody.AddForce(Vector2.right * _movementSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
         }
 
-        if(_spacePressed && _grounded){ // Jump
-            _playerBody.AddForce(Vector2.up * _jumpHeight, ForceMode2D.Impulse);
-            gameObject.GetComponent<ColourSwitch>().Switch();
-        }  
     }
 
     void GetUserInput(){
@@ -52,14 +49,22 @@ public class PlayerMovement : MonoBehaviour
         }
         else{
             _dPressed = false;
-        }
+        } 
+    }
 
-         if(Input.GetKey(KeyCode.Space)){ // detect spacebar being pressed
+    void Jump(){
+
+        if(Input.GetKeyUp(KeyCode.Space)){ // detect spacebar being pressed and released
             _spacePressed = true;
         }
         else{
             _spacePressed = false;
         }
+
+        if(_spacePressed && _grounded){ // Jump
+            _playerBody.AddForce(Vector2.up * _jumpHeight, ForceMode2D.Impulse);
+            gameObject.GetComponent<ColourSwitch>().Switch();
+        }  
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
