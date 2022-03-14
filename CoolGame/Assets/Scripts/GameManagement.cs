a3235938901bee5425eb08dd28d0e18dbaaec2f5
Sachin <sachinlaloo@gmail.com>
2022-03-14 21:40:17 +0200

    Added Scene Management
    Load next scene when end goal is reached

---
+++ CoolGame/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+    int currentLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextLevel(){
+        SceneManager.LoadScene(currentLevel+1);
+    }
+}
